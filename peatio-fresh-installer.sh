eb37c8caca2466e676a96e0917d6f62045cd8aba
Bitcoin Nero <40897884+BitcoinNero@users.noreply.github.com>
2019-01-19 07:01:48 +0100

    Update peatio-fresh-installer.sh

--- peatio-fresh-installer.sh
+++ peatio-fresh-installer.sh
@@ -27,8 +27,8 @@ git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-buil
 echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
-rbenv install --verbose 2.2.2
-rbenv global 2.2.2
+rbenv install --verbose 2.3.0
+rbenv global 2.3.0
 echo "gem: --no-document" > ~/.gemrc
 gem install bundler  -v '1.17.3'
 rbenv rehash
