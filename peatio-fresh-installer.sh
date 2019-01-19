f2f30fc656c7711441b9c50ed91b29a9b0c0bbd2
Bitcoin Nero <40897884+BitcoinNero@users.noreply.github.com>
2019-01-19 07:04:41 +0100

    Update peatio-fresh-installer.sh

--- peatio-fresh-installer.sh
+++ peatio-fresh-installer.sh
@@ -3,41 +3,30 @@
 echo -e "\n\n"
 echo -e "\033[34;7mBuild\e[0m"
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get -y install git-core curl zlib1g-dev build-essential boxes rbenv
 sudo apt-get -y install libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3
 sudo apt-get -y install libxml2-dev libxslt1-dev libcurl4-openssl-dev
-sudo apt-get -y install python-software-properties libffi-dev imagemagick gsfonts
-sudo apt-get -y install rabbitmq-server 
-sudo apt-get -y install redis-server nodejs
-sudo apt-get -y install  mysql-server  mysql-client  libmysqlclient-dev
-sudo rabbitmq-plugins enable rabbitmq_management
-sudo service rabbitmq-server restart
-wget http://localhost:15672/cli/rabbitmqadmin
-chmod +x rabbitmqadmin
-sudo mv rabbitmqadmin /usr/local/sbin
-
+sudo apt-get -y install python-software-properties libffi-dev imagemagick gsfonts rabbitmq-server 
+sudo apt-get -y install redis-server nodejs mysql-server  mysql-client  libmysqlclient-dev
+sudo rabbitmq-plugins enable rabbitmq_management && sudo service rabbitmq-server restart
+wget http://localhost:15672/cli/rabbitmqadmin && chmod +x rabbitmqadmin && sudo mv rabbitmqadmin /usr/local/sbin
 
 echo -e "\n\n"
-echo -e "\033[34;7mRuby Install\e[0m"
+echo -e "\033[34;7mRuby\e[0m"
 
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-rbenv install --verbose 2.3.0
-rbenv global 2.3.0
-echo "gem: --no-document" > ~/.gemrc
-gem install bundler  -v '1.17.3'
-rbenv rehash
+git clone git://github.com/sstephenson/rbenv.git .rbenv && git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile && echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile && rbenv install --verbose 2.3.0 && rbenv global 2.3.0
+echo "gem: --no-document" > ~/.gemrc && gem install bundler  -v '1.17.3' &&rbenv rehash
+
+echo -e "\n\n"
+echo -e "\033[34;7mNginx Passenger\e[0m"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
-sudo apt-get install -y nginx-extras passenger
+sudo apt-get update && sudo apt-get install -y nginx-extras passenger
 sudo rm /etc/nginx/passenger.conf
 touch /etc/nginx/passenger.conf
 cat <<EOF > /etc/nginx/passenger.conf
@@ -47,13 +36,13 @@ EOF
 sudo sed -i 's+# include /etc/nginx/passenger.conf;+include /etc/nginx/passenger.conf;+g' /etc/nginx/nginx.conf
 
 echo -e "\n\n"
-echo -e "\033[34;7mSetup production environment variable\e[0m"
+echo -e "\033[34;7mInstall\e[0m"
 
 echo "export RAILS_ENV=production" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/peatio
 cd peatio
-git clone https://github.com/algobasket/PeatioCryptoExchange.git .
+git clone https://github.com/BitcoinNero/PeatioCryptoExchange.git .
 bundle install --without development test --path vendor/bundle
 bin/init_config
 
